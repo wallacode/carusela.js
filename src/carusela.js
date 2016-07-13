70a134d4250e63e13ab6e708b40b41f44e9cd695
tal <tal_z@walla.net.il>
2016-07-13 11:23:25 +0300

    update

--- src/carusela.js
+++ src/carusela.js
@@ -1,8 +1,6 @@
-(function (window) {
+(function (_) {
 
     'use strict';
-
-    window['Carusela'] = window['Carusela'] || {};
 
     /**
      * Carusela.js
@@ -14,70 +12,131 @@
      * * Number of folds in scroll
      * * Current fold
      *
-     * @param element
      * @returns {{}}
      */
-    window['Carusela'] = function (element) {
+
+    var
+    /**
+     * Default values
+     * @type {object}
+     */
+    config = {
+        direction : 'rtl',
+        scrollingPer : 'element',
+        toggleBackward : false
+    };
+
+    _.Carusela = function (_config) {
+        if (_config) {
+            this.setConfig(_config);
+        }
+
         var wrapper         = document.createElement('div'),
             backwardElement = document.createElement('button'),
             forwardElement  = document.createElement('button'),
-
-            /**
-             * Get current position fold number
-             *
-             * @param scroll
-             * @param width
-             * @returns {number}
-             */
-            getCurrentFold = function(scroll, width) {
-                return Math.ceil(width / scroll);
-            },
-
-            /**
-             * Go x pixels backwards
-             *
-             * @returns {number}
-             */
-            backward = function() {
-                return wrapper.scrollLeft -= wrapper.offsetWidth;
-            },
-
-            /**
-             * Go x pixels forward
-             *
-             * @returns {number}
-             */
-            forward = function() {
-                return wrapper.scrollLeft += wrapper.offsetWidth;
-            }
+            element         = document.getElementById('demo'),
+            currenScrollPos = 0,
+            signDirection   = config.direction == 'ltr' ? '-' : '+',
+            counter         = document.getElementById('counter'),
+            index           = 1,
+            elemPerFold,
+            elemWidth,
+            scrollingPer,
+            totalElements
         ;
+        
+            wrapper.className           = 'carusela ' + config.direction;
+            backwardElement.innerText   = 'Backward';
+            backwardElement.className   = 'backward ' + config.direction;
+            forwardElement.innerText    = 'Forward';
+            forwardElement.className    = 'forward ' + config.direction;
 
-        wrapper.className           = 'carusela';
-        backwardElement.innerText   = 'Backward';
-        backwardElement.className   = 'backward';
-        forwardElement.innerText    = 'Forward';
-        forwardElement.className    = 'forward';
+            wrapper.setAttribute('dir', config.direction);
 
-        forwardElement.addEventListener('click', function() {
-            return forward();
-        });
+           setTimeout(function(){
+               // Bind image loading event
+                elemWidth         = element.children[1].offsetWidth + 10;
+                elemPerFold       = Math.floor(document.getElementsByClassName(wrapper.className)[0].offsetWidth / (elemWidth - 10));
+                totalElements     = elemWidth * (element.children.length - elemPerFold);
+                scrollingPer      = config.scrollingPer == 'element' ? 1 : elemPerFold;
 
-        backwardElement.addEventListener('click', function() {
-            return backward();
-        });
+                if (config.scrollingPer !== 'element') counter.innerHTML = index + '/' + elemPerFold;
+                if(config.toggleBackward) backwardElement.style.display = 'none';
 
-        // Add 'virtual' DOM elements
-        element.parentNode.replaceChild(wrapper, element);
-        wrapper.appendChild(element);
+           },0);
 
-        // Adding buttons
-        wrapper.parentNode.appendChild(backwardElement);
-        wrapper.parentNode.appendChild(forwardElement);
+        /**
+         * Build DOM structures
+         *
+         * @returns
+         * @public
+         */
+        this.init = function () {
+            // Add 'virtual' DOM elements
+            element.parentNode.replaceChild(wrapper, element);
+            wrapper.appendChild(element);
 
-        return {
-            'backward': backward,
-            'forward': forward
+            if (!__isTouchDevice()) {
+                wrapper.style.overflow = 'hidden';
+
+                // Adding buttons
+                wrapper.parentNode.appendChild(backwardElement);
+                wrapper.parentNode.appendChild(forwardElement);
+            }
+
+            __attachEvents();
         };
+
+        /**
+         * Attach events
+         *
+         * @returns
+         * @private
+         */
+        function __attachEvents() {
+            forwardElement.addEventListener('click', function () {
+                if(totalElements <= currenScrollPos * scrollingPer) return;
+                if (config.scrollingPer !== 'element') counter.innerHTML = ++index + '/' + elemPerFold;
+
+                backwardElement.style.display = 'block';
+                currenScrollPos += elemWidth;
+                element.style.transform = 'translateX(' + signDirection + currenScrollPos * scrollingPer + 'px)';
+                
+                if(totalElements <= currenScrollPos * scrollingPer) forwardElement.style.display = 'none';
+            });
+
+            backwardElement.addEventListener('click', function () {
+                if(currenScrollPos == 0) return;
+                if (config.scrollingPer !== 'element') counter.innerHTML = --index + '/' + elemPerFold;
+
+                forwardElement.style.display = 'block';
+                currenScrollPos -= elemWidth;
+                element.style.transform = 'translateX(' + signDirection + currenScrollPos * scrollingPer + 'px)';
+
+                if(currenScrollPos == 0) backwardElement.style.display = 'none';
+            });
+        }
+
+        /**
+         * Get information on touch devices
+         *
+         * @returns {boolean}
+         * @private
+         */
+        function __isTouchDevice() {
+            return 'ontouchstart' in window ||       // works on most browsers
+                    navigator.maxTouchPoints;       // works on IE10/11 and Surface
+        }
+        
+        return this;
+    };
+
+    _.Carusela.prototype.setConfig = function (_config) {
+        for (var k in _config) {
+            if (_config.hasOwnProperty(k)) {
+                config[k] = _config[k];
+            }
+        }
     };
 
 }(window));
